3f2f715f6d0d750c74e272bf89f6e25542984b36
kaustubh0201 <kaustubh1818@gmail.com>
2023-12-16 17:43:13 +0530

    chore: adding dependencies and functionality for api calls

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home, Dashboard, Register, Edit, Error, PrivateRoute } from './pages';
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <Home />
+        </Route>
+        <PrivateRoute path='/dashboard' exact>
+          <Dashboard />
+        </PrivateRoute>
+        <Route path='/register'>
+          <Register />
+        </Route>
+        <Route path='/edit/:id'>
+          <Edit />
+        </Route>
+
+        <Route path='*'>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
